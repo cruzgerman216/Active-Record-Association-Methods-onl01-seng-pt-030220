8cf5d5d9d809dcd6c2f4ffbecf524195d2e376b3
German Cruz <cruzgerman216@yahoo.com>
2020-05-05 23:40:35 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -13,6 +13,7 @@ class Song < ActiveRecord::Base
     getdrake = Artist.find_by(name: "drake")
     if getdrake == nil
      drake = Artist.new(name: "Drake")
+     binding.pry
      self.artist = drake
    else
      self.artist = getdrake
